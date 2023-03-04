a5d7fa9c836bb8d9d9a75784445d6970fdd65e55
Jacob G <jrgrant560@gmail.com>
2023-03-04 15:34:22 -0800

    014_String ends with

---
+++ 014_String ends with/script.js
@@ -0,0 +1,12 @@
+//Completion Time: ~5 minutes
+//BREAKDOWN: literally one method...
+
+// returns boolean if str ends with ending
+function solution(str, ending){
+    return str.endsWith(ending);
+}
+
+// ---------
+// let test = 12345;
+
+// console.log(func(test));
